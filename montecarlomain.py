58ae4883dc46944860214c080f4d40b329139e1d
JDBumgardner <jake.bumardner@gmail.com>
2015-12-14 06:18:19 +0000

    Fralie Nord

---
+++ montecarlomain.py
@@ -0,0 +1,19 @@
+mport montecarlo
+
+a = montecarlo.montecarlo()
+a.diffusion_tree()
+a.structure_geometry()
+a.rebar_calc()
+a.crack_calc()
+a.derating_calc()
+a.parameter_fill()
+a.monte_zeroes()
+a.monte_fill()
+a.histogram()
+a.outputs()
+a.normalization()
+a.plot_damage()
+
+print a.monte_bincount
+print a.x
+
